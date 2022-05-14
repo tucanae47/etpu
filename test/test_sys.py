2e904af49f8f09d227899895a6a518de53e74052
tucanae47 <cmsvalenzuela@gmail.com>
2022-05-15 00:06:08 +0200

    Format, correct data size

--- test/test_sys.py
+++ test/test_sys.py
@@ -29,14 +29,15 @@ async def RST(dut):
     dut.reset <= 0
     # await ClockCycles(dut.clk, 1)
 
-async def write_i(dut,data):
+
+async def write_i(dut, data):
     dut.input_i = BinaryValue(data)
     await RisingEdge(dut.clk)
 
-async def write_d(dut,data):
+
+async def write_d(dut, data):
     dut.data = BinaryValue(data)
     await RisingEdge(dut.clk)
-
 
 
 async def print_sys_out(sa):
@@ -45,6 +46,7 @@ async def print_sys_out(sa):
     # if sa.out.value.is_resolvable:
     #     result = sa.out.value
     #     print(">",result, result.buff)
+
 
 async def print_pe_out(out):
     if out.value.is_resolvable:
@@ -64,21 +66,20 @@ async def test_sys(dut):
         await RST(dut)
 
         ''' generate random input matrices bit len 5 for the moment , we need to transpose the weights before start '''
-        W = np.random.randint(8, size=(3,3),dtype=np.dtype(int) )
-        I = np.random.randint(8, size=(3,3),dtype=np.dtype(int) ).tolist()
-        # W = np.random.choice(list(range(1,255)), (3,3))
+        # W = np.random.randint(8, size=(3,3),dtype=np.dtype(int) )
+        # I = np.random.randint(8, size=(3,3),dtype=np.dtype(int) ).tolist()
+        W = np.random.choice(list(range(1, 128)), (3, 3))
         # W = W.astype(int)
-        # I = np.random.choice(list(range(1,255)), (3,3))
+        I = np.random.choice(list(range(1, 128)), (3, 3))
         # I = I.astype(int)
         '''We need to transpose the matrix as current processing arragement'''
         Wt = np.array(W).transpose().tolist()
         # Wt = Wt.astype(int)
 
-
         # print(W)
         # print(I)
 
-        expected = np.matmul(W,I)
+        expected = np.matmul(W, I)
         '''
         1. stage load waigths
         '''
@@ -92,32 +93,27 @@ async def test_sys(dut):
         dut.data = BinaryValue(w_data)
         await RisingEdge(dut.clk)
 
-
-
         '''
         2. stage run: stream the input matrix in diagonal shape: rustic mode for now :P 
         '''
         w_data = int(I[0][0])
         await write_i(dut, w_data)
-        w_data = int(I[1][0]) << 8 | int(I[0][1]) 
+        w_data = int(I[1][0]) << 8 | int(I[0][1])
         await write_i(dut, w_data)
         w_data = int(I[2][0]) << 16 | int(I[1][1]) << 8 | int(I[0][2])
         await write_i(dut, w_data)
         w_data = int(I[2][1]) << 16 | int(I[1][2]) << 8
         await write_i(dut, w_data)
-        w_data = int(I[2][2]) << 16 
+        w_data = int(I[2][2]) << 16
         await write_i(dut, w_data)
-        
 
         '''
         3. stage stop: collect data 
         '''
-        observed = np.zeros((3,3))
-        mask_0= (1<<16)-1
+        observed = np.zeros((3, 3))
+        mask_0 = (1 << 16)-1
         mask_1 = mask_0 << 16
-        # mask_2 = mask_0 << 20
-        masks = [mask_0,mask_1]
-        index = 0
+        masks = [mask_0, mask_1]
         values = []
         for i in range(12):
             ops = dut.ops.value.integer
@@ -125,24 +121,25 @@ async def test_sys(dut):
                 # value = dut.out.value.integer
                 value = await print_pe_out(dut.out)
                 if value is not None and value > 0:
-                    for i,mask in enumerate(masks):
+                    for i, mask in enumerate(masks):
                         ob = int((value & mask) >> (i*16))
                         values.append(ob)
-            
+
             await ClockCycles(dut.clk, 1)
+
         if len(values) < 9:
             print("failed----------------------------------->")
             print(W)
             print(I)
-            # assert(0)
+            assert(0)
         else:
             for k in range(9):
                 i = int(k / 3)
                 j = k % 3
                 observed[i][j] = values[k]
-            
-            # print(observed)
-            # print(expected)
+
+            print(observed)
+            print(expected)
             for i in range(3):
                 for j in range(3):
                     assert(observed[i][j] == expected[i][j])
